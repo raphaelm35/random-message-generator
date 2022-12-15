914d5e4f6864c89b8aba6de460387445b50297ca
Rafiki35 <raphael.masson35@gmail.com>
2022-12-15 10:04:56 +0100

    added the for loop for the DayMessage

--- main.js
+++ main.js
@@ -1,21 +1,46 @@
-const dayMessage = ['loves', 'eradicate', 'invented', 'codes', 'dine with', 'eat', 'wake up', 'drink', 'makes joke about', 'think about'];
+const dayMessage = ['Aliens', 'Santa', 'Your future grand-child', 'Your brother', 'Your 10th spouse', 'Everyone except you', 'Nobody', 'The Flying Spaghetti Monster', 'Your neighbor', 'Your boss', 'The pizza delveryman', 'Your future self'];
 
-const monthMessage = ['Aliens', 'Santa', 'Your future grand-child', 'Your brother', 'Your 10th spouse', 'Everyone except you', 'Nobody', 'The Flying Spaghetti Monster', 'Your neighbor', 'Your boss', 'The pizza delveryman', 'Your future self'];
+const monthMessage = ['loves', 'eradicate', 'invented', 'codes', 'dine with', 'eat', 'wake up', 'drink', 'makes joke about', 'think about'];
 
-const decadeMessage = ['bitcoin', 'seals', 'global warming', 'codecademy', 'kinden garden drawings', 'turtles', 'water', 'triple jalapenos pizza'];
+const decadeMessage = ['bitcoin', 'seals', 'Ninja Turtles', 'codecademy', 'kinden garden drawings', 'turtles', 'alcohol', 'triple jalapenos pizza'];
 
 const RandomMessage = (dayOfBirth, monthOfBirth, decadeOfBirth) => {
-    let returnedMonthMessage = "";
     let returnedDayMessage = "";
+    let returnedMonthMessage = "";
     let returnedDecadeMessage = "";
 
     if (dayOfBirth > 0 && dayOfBirth < 31 && monthOfBirth > 0 && monthOfBirth < 13 && decadeOfBirth > 1940 && decadeOfBirth < 2023){
-        if (dayOfBirth < 5){
-            console.log("it works")
+        for (i = 0 ; i < 31 ; i++){
+            if (dayOfBirth < 3){
+                returnedDayMessage = dayMessage[0];
+            } else if (dayOfBirth >= 3 && dayOfBirth <=5){
+                returnedDayMessage = dayMessage[1];
+            } else if (dayOfBirth >= 6 && dayOfBirth <=8){
+                returnedDayMessage = dayMessage[2];
+            } else if (dayOfBirth >= 9 && dayOfBirth <=11){
+                returnedDayMessage = dayMessage[3];
+            } else if (dayOfBirth >= 12 && dayOfBirth <=14){
+                returnedDayMessage = dayMessage[4];
+            } else if (dayOfBirth >= 15 && dayOfBirth <=17){
+                returnedDayMessage = dayMessage[5];
+            } else if (dayOfBirth >= 18 && dayOfBirth <=20){
+                returnedDayMessage = dayMessage[6];
+            } else if (dayOfBirth >= 21 && dayOfBirth <=23){
+                returnedDayMessage = dayMessage[7];
+            } else if (dayOfBirth >= 24 && dayOfBirth <=26){
+                returnedDayMessage = dayMessage[8];
+            } else if (dayOfBirth >= 27 && dayOfBirth <=29){
+                returnedDayMessage = dayMessage[9];
+            } else if (dayOfBirth >= 30 && dayOfBirth <=31){
+                returnedDayMessage = dayMessage[10];
+            }
+        };
+        for (j=0 ; j<13 ; j++) {
+            if (monthOfBirth === 1){
+                returnedMonthMessage = monthMessage[0];
+            };
         }
-        
-
-
+    console.log(`The undeniable truth is that ${returnedDayMessage} ${returnedMonthMessage}`);
     } else if (dayOfBirth < 0 && dayOfBirth > 31) {
         console.log("Please return valid date of birth")
     } else if (monthOfBirth < 0 && monthOfBirth >= 13) {
@@ -28,4 +53,4 @@ const RandomMessage = (dayOfBirth, monthOfBirth, decadeOfBirth) => {
 }
 
 
-RandomMessage(4, 3, 1999);
+RandomMessage(22, 1, 1999);
